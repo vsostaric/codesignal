fb3d3a9627cfd606906b500c30b0993fa2265005
vladimir <vladimirsostaric3390@gmail.com>
2019-01-16 17:33:07 +0100

    ChessBoardCellColor

---
+++ jvm/codefights-kotlin/src/arcade/ChessBoardCellColor.kt
@@ -0,0 +1,10 @@
+package arcade
+
+fun chessBoardCellColor(cell1: String, cell2: String): Boolean {
+    return ((cell1[1].toInt() + cell1[0].toInt()) % 2) == ((cell2[1].toInt() + cell2[0].toInt()) % 2)
+}
+
+fun main(args: Array<String>) {
+    System.out.println(chessBoardCellColor("A1", "C3").toString() + " ---> Should be true")
+    System.out.println(chessBoardCellColor("A1", "H3").toString() + " ---> Should be false")
+}
