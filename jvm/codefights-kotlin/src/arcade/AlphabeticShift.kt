7dad8c467967e125b8fb7a906a7051808f44518c
vladimir <vladimirsostaric3390@gmail.com>
2019-01-16 17:20:48 +0100

    solve alphabetic shift

---
+++ jvm/codefights-kotlin/src/arcade/AlphabeticShift.kt
@@ -0,0 +1,17 @@
+package arcade
+
+fun alphabeticShift(inputString: String): String {
+    return inputString
+            .map { character -> character.toInt() }
+            .map { asciiCode ->
+                if (asciiCode != 122) (asciiCode + 1) else 97
+            }
+            .map { asciiCode -> asciiCode.toChar() }.joinToString("")
+
+}
+
+
+fun main(args: Array<String>) {
+    System.out.println(alphabeticShift("crazy") + " ---> Should be dsbaz")
+    System.out.println(alphabeticShift("z") + " ---> Should be a")
+}
